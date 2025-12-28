da73ed25100c67622fcd74448136ba1c4ecaffaf
Sham <shamsharan00@gmail.com>
2025-12-28 18:38:41 +0530

    Basic_program Odd orr even  program

--- basic_program/odd_even.c
+++ basic_program/odd_even.c
@@ -1,3 +1,17 @@
+/*
+Given a number n, check whether it is even or odd. Return true for even and false for odd.
+
+Examples: 
+
+Input: n = 15
+Output: false
+Explanation: 15 % 2 = 1, so 15 is odd .
+
+Input: n = 44
+Output: true
+Explanation: 44 % 2 = 0, so 44 is even.
+*/
+
 #include<stdio.h>
 int main() {
     int number;
