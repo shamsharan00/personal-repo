b61fe587e8c0c343df8ae167376aeafa47cbb31e
Sham <shamsharan00@gmail.com>
2026-01-18 17:05:46 +0530

    Reverse the single linked linked_list

--- single_linked_list/linked_list.c
+++ single_linked_list/linked_list.c
@@ -6,6 +6,7 @@ function we need to perform on linked list
 #include "insert.h"
 #include "deletion.h"
 #include "search.h"
+#include "reverse.h"
 
 int main() {
     struct node * head = NULL;
@@ -13,7 +14,8 @@ int main() {
 
     do {
         printf("1. create node\n2. print linked list \n3. insert node by position"
-            "\n4. delete node \n5. search node by position\n6. search node by data\n7.exit \n"); 
+            "\n4. delete node \n5. search node by position\n6. search node by data\n7. "
+            "reverse\n8. exit \n"); 
         printf(" Enter the number to do the operations on linked list \n");
         scanf("%d", &option);
         switch (option) {
@@ -36,6 +38,9 @@ int main() {
                 search_node_data(head);
                 break;
             case 7:
+                head = reverse(head);
+                break;
+            case 8:
                 exit(0);
             default:
                 printf(" please enter the valid option \n");
--- single_linked_list/reverse.h
+++ single_linked_list/reverse.h
@@ -1,3 +1,11 @@
 struct node *reverse(struct node *head) {
-    
+    struct node *prev = NULL, *next = NULL, *curr = head;
+
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
 }
